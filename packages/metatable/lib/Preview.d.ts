9b7be71387dd00a21ed47e483be8f597cd0acf42
Honza Smid <jansmidhonza@gmail.com>
2020-03-05 17:15:51 +0100

    fixed dependencies

--- packages/metatable/lib/Preview.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import './App.css';
-declare const Preview: React.FC;
-export default Preview;
--- packages/metatable/lib/components/MetaTable.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const MetaTable: () => JSX.Element;
-export default MetaTable;
