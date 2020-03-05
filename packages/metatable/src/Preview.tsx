bce4d0e25cf3677a0960f68fd99536b2071ff757
Honza Smid <jansmidhonza@gmail.com>
2020-03-05 17:16:05 +0100

    0.0.2

--- packages/metatable/src/Preview.tsx
+++ packages/metatable/src/Preview.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import MetaTable from './exports';
 
 const Preview: React.FC = () => {
-    return (
-        <div>
-            <h1>MetaTable</h1>
-            <MetaTable
-                columns={[
-                    {
-                        label: 'Id',
-                    },
-                    {
-                        label: 'FirstName',
-                        render: (value) => `name: ${value}`
-                    },
-                    {
-                        label: 'actions',
-                        render: (cell, row) => `action 1`
-                    }
-                ]}
-                data={[
-                    [4, 'Tom', 'x', 'y'],
-                    [9, 'Joseph']
-                ]}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h1>MetaTable</h1>
+      <MetaTable
+        columns={[
+          {
+            label: 'Id',
+          },
+          {
+            label: 'FirstName',
+            render: value => `name: ${value}`,
+          },
+          {
+            label: 'actions',
+            render: (cell, row) => `action 1`,
+          },
+        ]}
+        data={[
+          [4, 'Tom', 'x', 'y'],
+          [9, 'Joseph'],
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Preview;
--- packages/metatable/src/exports.tsx
+++ packages/metatable/src/exports.tsx
@@ -1,1 +1,1 @@
-export { default } from './components/MetaTable'
+export { default } from './components/MetaTable';
