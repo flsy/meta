27fb463b5802872b68a44bbcc479ddd8f33b1e64
davidtourek <dtourek@centrum.cz>
2021-07-23 11:25:56 +0200

    strings filter intro

--- packages/metafilters/src/__tests__/filters.spec.ts
+++ packages/metafilters/src/__tests__/filters.spec.ts
@@ -158,6 +158,114 @@ describe('filters', () => {
     });
   });
 
+  describe('strings filter', () => {
+    it('should return all when undefined in filter', async () => {
+      const db = await seed();
+      const response = await metafilters(exampleColumn, 'person-dash', {
+        filters: {
+          firstName: {
+            type: 'strings',
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            filters: undefined,
+          },
+        },
+      });
+
+      const count = await get(db, response.count);
+      const nodes = await all(db, response.nodes);
+      await close(db);
+
+      expect(response).toEqual({
+        count: 'SELECT COUNT(*) as count FROM "person-dash";',
+        nodes: 'SELECT "id", "firstName", "lastName", "age", "isValid" FROM "person-dash";',
+      });
+
+      expect(count).toEqual({ count: 4 });
+      expect(nodes.length).toEqual(4);
+    });
+
+    it('should filter', async () => {
+      const db = await seed();
+      const response = await metafilters(exampleColumn, 'person-dash', {
+        filters: {
+          firstName: {
+            type: 'strings',
+            filters: [
+              { value: 'Beta', operator: 'EQ' },
+              { value: 'Joe', operator: 'EQ' },
+            ],
+          },
+        },
+      });
+
+      const count = await get(db, response.count);
+      const nodes = await all(db, response.nodes);
+      await close(db);
+
+      expect(response).toEqual({
+        count: 'SELECT COUNT(*) as count FROM "person-dash" WHERE "firstName" = \'Beta\' OR "firstName" = \'Joe\';',
+        nodes:
+          'SELECT "id", "firstName", "lastName", "age", "isValid" FROM "person-dash" WHERE "firstName" = \'Beta\' OR "firstName" = \'Joe\';',
+      });
+      expect(count).toEqual({ count: 2 });
+      expect(nodes).toMatchObject([{ firstName: 'Joe' }, { firstName: 'Beta' }]);
+    });
+
+    it('should filter with operators', async () => {
+      const db = await seed();
+      const response = await metafilters(exampleColumn, 'person-dash', {
+        filters: {
+          firstName: {
+            type: 'strings',
+            filters: [{ value: 'e' }, { value: 'a' }],
+          },
+        },
+      });
+
+      expect(response).toEqual({
+        count: 'SELECT COUNT(*) as count FROM "person-dash" WHERE "firstName" like \'%e%\' OR "firstName" like \'%a%\';',
+        nodes:
+          'SELECT "id", "firstName", "lastName", "age", "isValid" FROM "person-dash" WHERE "firstName" like \'%e%\' OR "firstName" like \'%a%\';',
+      });
+
+      const count = await get(db, response.count);
+      const nodes = await all(db, response.nodes);
+      await close(db);
+
+      expect(count).toEqual({ count: 4 });
+      expect(nodes.length).toEqual(4);
+    });
+
+    it('should filter with functions', async () => {
+      const db = await seed();
+      const response = await metafilters(exampleColumn, 'person-dash', {
+        filters: {
+          firstName: {
+            type: 'strings',
+            filters: [
+              { value: 'joe', operator: 'EQ', customFunction: 'lower' },
+              { value: 'Beta', operator: 'EQ' },
+            ],
+          },
+        },
+      });
+
+      expect(response).toEqual({
+        count: 'SELECT COUNT(*) as count FROM "person-dash" WHERE lower("firstName") = \'joe\' OR "firstName" = \'Beta\';',
+        nodes:
+          'SELECT "id", "firstName", "lastName", "age", "isValid" FROM "person-dash" WHERE lower("firstName") = \'joe\' OR "firstName" = \'Beta\';',
+      });
+
+      const count = await get(db, response.count);
+      const nodes = await all(db, response.nodes);
+      await close(db);
+
+      expect(count).toEqual({ count: 2 });
+      expect(nodes).toMatchObject([{ firstName: 'Joe' }, { firstName: 'Beta' }]);
+    });
+  });
+
   describe('filters combined', () => {
     it('filter', async () => {
       const db = await seed();
