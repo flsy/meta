62647cf406b7478da847434ac25756deeab71a18
Jakub Kolebaba <jakub@kolebaba.cz>
2021-10-12 17:42:35 +0200

    Set values to form

--- packages/react-metaforms/src/Form.tsx
+++ packages/react-metaforms/src/Form.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import { Form, Formik, FormikHelpers, FormikProps, useField } from 'formik'
 import { FieldHelperProps, FieldInputProps, FieldMetaProps } from 'formik/dist/types'
+import { lensPath, view } from 'ramda';
 
 export interface MetaField {
   name: string,
@@ -14,6 +15,13 @@ interface IProps {
   fields: MetaField[];
   onSubmit: (values: any, helpers: FormikHelpers<any>) => void;
   components: (q: { ref: any, field: MetaField, input: FieldInputProps<any>, meta: FieldMetaProps<any>, helpers: FieldHelperProps<any> } ) => JSX.Element;
+}
+
+const setValues = (values: any, fields: MetaField[]) => {
+  return fields.map(f => {
+    const nameLens = lensPath(f.name.split('.'));
+    return { ...f, value: view(nameLens, values) };
+  });
 }
 
 export default (props: IProps) => {
@@ -32,16 +40,16 @@ export default (props: IProps) => {
     return props.components({ field, ref, input, meta, helpers });
   }
 
+
   return (
     <Formik
       initialValues={{}}
-      validate={(v) => {
-        console.log('validation', v);
-      }}
+      // validate={}
       onSubmit={props.onSubmit}
     >
       {(fp: FormikProps<any>) => (
         <Form>
+          <div>{JSON.stringify(setValues(fp.values, props.fields))}</div>
           {fp.isSubmitting && <>submitting</>}
           {props.fields.map((field => (<Field key={field.name} field={field} />)))}
         </Form>
