bddd1fa334c7ff278ce70caf2a8ac3cdecff9f1f
davidtourek <dtourek@centrum.cz>
2021-12-17 11:03:12 +0100

    feat(metacore) added textNoWrap into metatable interface

--- packages/metacore/src/metatable/interfaces.ts
+++ packages/metacore/src/metatable/interfaces.ts
@@ -1,5 +1,5 @@
 import { MetaField } from '../metaforms/interfaces';
 
-export type Column<TTypes> = { type?: TTypes; label?: string; key?: boolean; isOmitted?: boolean; filterForm?: MetaField[]; sortForm?: MetaField[] };
+export type Column<TTypes> = { type?: TTypes; label?: string; key?: boolean; isOmitted?: boolean; filterForm?: MetaField[]; sortForm?: MetaField[]; textNoWrap?: boolean };
 export type Columns<TTypes> = { [key: string]: Column<TTypes> | Columns<TTypes> };
 export type OneOrMany<T> = T | Array<T>;
