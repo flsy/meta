e1925e8c970e505d6fc046696157641ca14814b0
Jakub Kolebaba <jakub@kolebaba.cz>
2022-09-03 08:50:31 +1000

    Export only necessary types and Form from react-metaforms

--- packages/react-metaforms/src/export.ts
+++ packages/react-metaforms/src/export.ts
@@ -1,3 +1,2 @@
-export { default, isComponentObject, isComponentArray, isComponentControl } from './core/Form';
-export type { IProps, ComponentRenderProps, ControlRenderProps, ObjectRenderProps, ArrayRenderProps } from './core/Form';
-export { default as AntdForm } from './antd/Form'
+export type { IProps } from './core/Form';
+export { default } from './antd/Form'
