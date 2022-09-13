65569276ba8d3bd01609d401bb859f0aa98fb267
Honza Smid <jansmidhonza@gmail.com>
2022-09-13 13:01:13 +0200

    exclude submit field from validating

--- packages/metaforms/src/validate/validateForm.ts
+++ packages/metaforms/src/validate/validateForm.ts
@@ -3,13 +3,15 @@
 import { MetaField } from "@falsy/metacore";
 import { validate } from "./validate";
 import {Optional} from "fputils";
+import {isSubmit} from "react-metaforms/lib/antd/utils";
 
 type MetaError = { [name: string]: string }
 type MetaErrors = { [name: string]: (string | MetaError) }
 
 
 const getErrorMessage = (formData: any | undefined, field: MetaField, value: any): Optional<string> => {
-   const errorMessages = (field.validation || []).map((rule) => validate(value, rule, formData)).filter((error) => error);
+   // submit does  not have validation field
+   const errorMessages = (!isSubmit(field) && field.validation || []).map((rule) => validate(value, rule, formData)).filter((error) => error);
    return errorMessages.length > 0 ? errorMessages[0] : undefined;
 }
 
